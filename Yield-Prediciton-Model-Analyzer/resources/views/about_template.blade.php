bf4adb566b9c1ebb6109fdcbfe022b63cfe68aa9
Devansh-Trivedi <tridevansh.160601@gmail.com>
2022-10-08 02:03:05 +0530

    yield analyzer model

---
+++ Yield-Prediciton-Model-Analyzer/resources/views/about_template.blade.php
@@ -0,0 +1,63 @@
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+
+
+            <p align="center">
+                <img  class="img-responsive" src="http://www.indiawaterportal.org/sites/indiawaterportal.org/files/KSY.jpg">
+            </p>
+
+            <p align="center">
+                <a><img src="https://poser.pugx.org/laravel/framework/license.svg" alt="License"></a>
+            </p>
+            <br>
+            <div class="panel panel-info">
+                <div class="panel-body"><h4>See Project at github <i class="fa fa-github"></i></h4>
+                    <ul class="list-inline" style="font-size: 1.1em">
+                        <li ><a target='_blank' href="http://github.com/ashishpatel0720/kisanmitra" class="label label-primary">Web Application Part</a></li>
+                        <li ><a target='_blank' href="http://github.com/ashishpatel0720/kisanmitra-python" class="label label-danger">Machine Learning Part</a></li>
+                    </ul>
+                    <a href="http://github.com/ashishpatel0720/kisanmitra">
+
+                    </a></div>
+            </div>
+            <br>
+
+            <h2 id="heading">About Kisanmitra</h2>
+            <p>Kisan is a machine learning based web application with expressive, elegant visualization of yield predictions. We believe development of a country is possible only with the technical advancement of farmers, an educated farmer can do way better than a experienced uneducated one, because he can use modern technology to make proper planning about his/her future. using kisanmitra , one can predict the crop yield for next 5 year.</p>
+            <ul>
+                <li><a href="https://kisanmitra.co/predict">Simple, fast Algorithm</a>.</li>
+                <li>Expressive, intuitive visualization Using Charts.</li>
+                <li><a href="https://kisanmitra.co/about">Robust background machine learning</a>.</li
+                <li><a href="https://kisanmitra.com/team">Hardworking Team</a>.</li>
+            </ul>
+            <p>kisanmitra is accessible, yet powerful tools needed for crop yeild production. A superb combination of simplicity, elegance, and innovation give you tools you need to properly regulate your farming strategies according to environment and market.</p>
+            <h2 id="technology-used" class="ap-heading" style="margin:1em">technology used</h2>
+
+            <a target='_blank' href="http://laravel.com"><img width='250' height='200'  class="img-thumbnail" src="https://camo.githubusercontent.com/5ceadc94fd40688144b193fd8ece2b805d79ca9b/68747470733a2f2f6c61726176656c2e636f6d2f6173736574732f696d672f636f6d706f6e656e74732f6c6f676f2d6c61726176656c2e737667">
+            </a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <a target='_blank' href="http://scikit-learn.org"><img width='250' height='100' class="img-thumbnail" src="http://www.scipy-lectures.org/_images/scikit-learn-logo.png">
+            </a>
+
+            <h2 id="contributing" class="ap-heading" style="margin:1em">Contributing</h2>
+            <div class="panel panel-info">
+                <div class="panel-body"><h4>See Project at github  <i class="fa fa-github"></i></h4>
+                    <ul class="list-inline" style="font-size: 1.1em">
+                        <li ><a target='_blank' href="http://github.com/ashishpatel0720/kisanmitra" class="label label-primary">Web Application Part</a></li>
+                        <li ><a target='_blank' href="http://github.com/ashishpatel0720/kisanmitra-python" class="label label-danger">Machine Learning Part</a></li>
+                    </ul>
+                    <a target='_blank' href="http://github.com/ashishpatel0720/kisanmitra">
+
+                    </a></div>
+            </div>
+            <p>You are welcome to contribute to this project, Thank you for considering contributing for the greater good for farmers!
+            <p>If you discover a vulnerability within kisanmitra, please send an e-mail to Ashish Patel at ashishpatel0720@gmail.com. All vulnerabilities will be promptly addressed.</p>
+            <h2 id="license" class="ap-heading" style="margin:1em">License</h2>
+            <p>kisanmitra is open-sourced software licensed under the <a target='_blank'  href="http://opensource.org/licenses/MIT">MIT license</a>.</p>
+
+
+        </div>
+    </div>
+</div>
